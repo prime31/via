a04b1d8c01c0af5e01279dfe03ea6112bf274b2b
Mike <mike@prime31.com>
2020-01-05 23:19:12 -0800

    cleansed of sapp

--- via_config.v
+++ via_config.v
@@ -11,12 +11,4 @@ pub mut:
 	window_fullscreen bool = false			/* whether the window should be created in fullscreen mode */
 	window_vsync bool = true
 	window_highdpi bool = false				/* whether the rendering canvas is full-resolution on HighDPI displays */
-
-    sample_count int                   		/* MSAA sample count */
-    swap_interval int                  		/* the preferred swap interval (ignored on some platforms) */
-    alpha bool                         		/* whether the framebuffer should have an alpha channel (ignored on some platforms) */
-    // window_title string = 'V is for via'	/* the window title as UTF-8 encoded string */
-    user_cursor bool                   		/* if true, user is expected to manage cursor image in SAPP_EVENTTYPE_UPDATE_CURSOR */
-    enable_clipboard bool              		/* enable clipboard access, default is false */
-    clipboard_size int                 		/* max size of clipboard content in bytes */
 }
