a9bd433e678eeb30f67afe60b24308f169617e61
Mike <mike@prime31.com>
2020-01-08 21:47:04 -0800

    new randoms

---
+++ math/rand.v
@@ -0,0 +1,22 @@
+module math
+
+[inline]
+pub fn rand() f32 {
+	return f32(C.rand()) / C.RAND_MAX
+}
+
+[inline]
+pub fn rand_between(min, max f32) f32 {
+	return f32(f32(C.rand()) / C.RAND_MAX) * (max - min) + min
+}
+
+[inline]
+pub fn rand_int() int {
+	return C.rand()
+}
+
+[inline]
+pub fn rand_int_next(max int) int {
+	return C.rand() % max
+}
+
