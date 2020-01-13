a747804bf2f614174142aa6b7af8a417421e6d34
Mike Desaro <mike@prime31.com>
2020-01-13 11:47:37 -0800

    physfs to platform specific folder

--- libs/physfs/c_physfs.v
+++ libs/physfs/c_physfs.v
@@ -3,9 +3,9 @@ module physfs
 #flag -I @VMOD/via/libs/physfs/thirdparty
 
 #flag darwin -framework IOKit -framework Foundation
-#flag darwin @VMOD/via/libs/physfs/thirdparty/libphysfs.a
+#flag darwin @VMOD/via/libs/physfs/thirdparty/macos/libphysfs.a
 
-#flag linux @VMOD/via/libs/physfs/thirdparty/libphysfs.a
+#flag linux @VMOD/via/libs/physfs/thirdparty/linux/libphysfs.a
 
 #include "physfs.h"
 
