9ebb857c3aa41a64d7af3914f7df9096a0e0f21a
Mike Desaro <mike@prime31.com>
2020-01-28 17:17:35 -0800

    junk

--- graphics/default_shader_src_metal.v
+++ graphics/default_shader_src_metal.v
@@ -9,44 +9,6 @@ const (
 
 using namespace metal;
 
-template<typename T, size_t Num>
-struct spvUnsafeArray
-{
-    T elements[Num ? Num : 1];
-    
-    thread T& operator [] (size_t pos) thread
-    {
-        return elements[pos];
-    }
-    constexpr const thread T& operator [] (size_t pos) const thread
-    {
-        return elements[pos];
-    }
-    
-    device T& operator [] (size_t pos) device
-    {
-        return elements[pos];
-    }
-    constexpr const device T& operator [] (size_t pos) const device
-    {
-        return elements[pos];
-    }
-    
-    constexpr const constant T& operator [] (size_t pos) const constant
-    {
-        return elements[pos];
-    }
-    
-    threadgroup T& operator [] (size_t pos) threadgroup
-    {
-        return elements[pos];
-    }
-    constexpr const threadgroup T& operator [] (size_t pos) const threadgroup
-    {
-        return elements[pos];
-    }
-};
-
 struct vs_in
 {
     float2 VertPosition [[attribute(0)]];
@@ -67,14 +29,14 @@ float4 position(thread const float3x2& transMat, thread const float2& localPosit
     return float4(transMat * float3(localPosition, 1.0), 0.0, 1.0);
 }
 
-vertex vs_out _main(vs_in in [[stage_in]], constant spvUnsafeArray<float3, 2>& TransformMatrix [[buffer(0)]])
+vertex vs_out _main(vs_in in [[stage_in]], constant array<float3, 2>& TransformMatrix [[buffer(0)]])
 {
     vs_out out = {};
     out.VaryingTexCoord = in.VertTexCoord;
     out.VaryingColor = in.VertColor;
-    float3x2 param = float3x2(float2(TransformMatrix[0].x, TransformMatrix[0].y), float2(TransformMatrix[0].z, TransformMatrix[1].x), float2(TransformMatrix[1].y, TransformMatrix[1].z));
-    float2 param_1 = in.VertPosition;
-    out.gl_Position = position(param, param_1);
+    float3x2 matrix = float3x2(TransformMatrix[0].x, TransformMatrix[0].y, TransformMatrix[0].z, TransformMatrix[1].x, TransformMatrix[1].y, TransformMatrix[1].z);
+    out.gl_Position = position(matrix, in.VertPosition);
+    // out.gl_Position = float4(in.VertPosition, 0.0, 1.0);
     return out;
 }'
 	default_vert_main_metal = ''
