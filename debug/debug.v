77ed3351af1c89af948a9bea49afc6711b9ce2fb
Mike Desaro <mike@prime31.com>
2020-02-05 11:50:48 -0800

    debug log

--- debug/debug.v
+++ debug/debug.v
@@ -172,3 +172,27 @@ pub fn draw_text(x, y f32, text string, config TextConfig) {
 }
 
 //#endregion
+
+//#region Logging
+
+pub fn info(msg string) {
+	print('\x1b[100m \x1b[49m ')
+	println(msg)
+}
+
+pub fn log(msg string) {
+	print('\x1b[42m \x1b[49m ')
+	println(msg)
+}
+
+pub fn warn(msg string) {
+	print('\x1b[43m \x1b[49m ')
+	println(msg)
+}
+
+pub fn error(msg string) {
+	print('\x1b[41m \x1b[49m ')
+	println(msg)
+}
+
+//#endregion
