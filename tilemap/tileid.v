6121a7d3668e3318e37fae226e8fc2211bcacfc5
Mike Desaro <mike@prime31.com>
2020-02-20 13:34:24 -0800

    rename

--- tilemap/tileid.v
+++ tilemap/tileid.v
@@ -13,28 +13,3 @@ pub fn (t TileId) flipped_h() bool { return (int(t) & flipped_h) != 0 }
 pub fn (t TileId) flipped_v() bool { return (int(t) & flipped_v) != 0 }
 
 pub fn (t TileId) flipped_d() bool { return (int(t) & flipped_d) != 0 }
-
-
-
-
-
-
-// POSSIBLY JUNK
-
-pub struct TileInfo {
-	id int
-	flip_h bool
-	flip_v bool
-	flip_d bool
-}
-
-fn tileinfo(id int) TileInfo {
-	mut t := TileInfo{
-		id: id & ~(flipped_h | flipped_v | flipped_d)
-		flip_h: (id & flipped_h) != 0
-		flip_v: (id & flipped_v) != 0
-		flip_d: (id & flipped_d) != 0
-	}
-
-	return t
-}
