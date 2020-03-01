236c69ea0ef468c88c448362d04873eb5598681b
Mike <mike@prime31.com>
2020-02-29 17:34:36 -0800

    killed fmod studio

--- libs/fmod/c_fmod.v
+++ libs/fmod/c_fmod.v
@@ -1,21 +1,18 @@
 module fmod
 import via.libs.fmod.core.c as c_core
-import via.libs.fmod.studio.c as c_studio
 
-pub const ( used_import = c_core.used_import + c_studio.used_import )
+pub const ( used_import = c_core.used_import )
 
 #flag -I @VROOT/libs/fmod/thirdparty/core
-#flag -I @VROOT/libs/fmod/thirdparty/studio
 
-#flag darwin -L @VROOT/libs/fmod/thirdparty/macos -lfmod -lfmodstudio
+#flag darwin -L @VROOT/libs/fmod/thirdparty/macos -lfmod
 #flag darwin -rpath @VROOT/libs/fmod/thirdparty/macos
 
 #flag linux -L @VROOT/libs/fmod/thirdparty/linux
 #flag linux -Wl,-rpath,@VROOT/libs/fmod/thirdparty/linux
-#flag linux -lfmod -lfmodstudio
+#flag linux -lfmod
 
 #include "fmod.h"
-#include "fmod_studio.h"
 #include "fmod_errors.h"
 
 
